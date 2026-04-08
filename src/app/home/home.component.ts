eb543db4fcc1c138fcb47b5180f5d333660dbe9f
Serkan Turhan <serkan.turhan@doverfs.com>
2026-04-08 16:01:33 -0500

    first pinpad version

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -3,6 +3,7 @@ import { GreetingComponent } from '../components/greeting/greeting.component';
 import { CounterComponent } from '../components/counter/counter.component';
 import { PulserviewComponent } from "../components/pulserview/pulserview.component";
 import { AppComponent } from "../app.component";
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-home',
@@ -11,7 +12,26 @@ import { AppComponent } from "../app.component";
   styleUrl: './home.component.scss'
 })
 export class HomeComponent {
+  isPinMode = false;
+  pinValue: string = ''; // Stores the actual numbers
+
+  toggleMode() {
+    this.isPinMode = !this.isPinMode;
+    this.pinValue = ''; // Clear PIN when switching modes
+  }
+
+  handleInput(val: string) {
+    if (this.pinValue.length < 4) { // Limit to 4 digits
+      this.pinValue += val;
+    }
+  }
+
+  clearPin() {
+    this.pinValue = '';
+  }
+
   homeMessage = signal('Serkan was here!');
+
   keyupHandler(event: KeyboardEvent) {
     console.log(`typed ${event.key} key`);
   }
