c980c5d5a71136daa4f8ec1530cc6af9a1bcf663
Serkan Turhan <serkan.turhan@doverfs.com>
2026-04-09 08:54:36 -0500

    shake feature

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,9 +1,8 @@
-import { Component, signal } from '@angular/core';
+import { Component, signal,viewChild } from '@angular/core';
 import { GreetingComponent } from '../components/greeting/greeting.component';
 import { CounterComponent } from '../components/counter/counter.component';
 import { PulserviewComponent } from "../components/pulserview/pulserview.component";
 import { AppComponent } from "../app.component";
-import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-home',
@@ -12,18 +11,56 @@ import { Router } from '@angular/router';
   styleUrl: './home.component.scss'
 })
 export class HomeComponent {
+
+  pulser = viewChild(PulserviewComponent);
+
   isPinMode = false;
-  pinValue: string = ''; // Stores the actual numbers
+  isAuthorized = false;
+  pinValue: string = '';
+  shakeLabel = false; // New flag for the shake animation
 
   toggleMode() {
     this.isPinMode = !this.isPinMode;
-    this.pinValue = ''; // Clear PIN when switching modes
+    this.isAuthorized = false;
+    this.pinValue = '';
+    this.shakeLabel = false;
+    if(!this.isPinMode){
+      this.pulser()?.stopPump();
+    }
   }
 
   handleInput(val: string) {
-    if (this.pinValue.length < 4) { // Limit to 4 digits
+    if (this.pinValue.length < 4) {
       this.pinValue += val;
+      if(this.pinValue.length == 4)
+      {
+          this.submitPin();
+      }
     }
+  }
+
+  submitPin() {
+    if (this.pinValue === '1236') {
+      this.isAuthorized = true;
+      this.pulser()?.startPump();
+    } else {
+      // Trigger Shake instead of Alert
+      this.triggerShake();
+      this.pinValue = ''; // Optional: clear pin on wrong attempt
+      this.pulser()?.stopPump();
+    }
+  }
+  handleLogout() {
+    this.isAuthorized = false;
+    this.pulser()?.stopPump();
+  }
+
+  triggerShake() {
+    this.shakeLabel = true;
+    // Remove the class after animation completes (500ms) so it can be re-triggered
+    setTimeout(() => {
+      this.shakeLabel = false;
+    }, 500);
   }
 
   clearPin() {
